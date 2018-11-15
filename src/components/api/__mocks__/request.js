31460f706a8f31c72a5c16cab119d9ab03b1b67b
Owen Jones <o-w-jones@outlook.com>
2018-11-15 22:48:56 +0000

    Update api test to properly capture component state change

--- src/components/api/__mocks__/request.js
+++ src/components/api/__mocks__/request.js
@@ -1,7 +1,7 @@
 import requestData from './requestData.json'
 
-export default jest.fn().mockImplementationOnce(() => {
-    donations: [
-        requestData
-    ]
+export default jest.fn().mockImplementation(() => {
+    return {
+        donations: [requestData]
+    }
 });
--- src/tests/apiDataToListState.test.js
+++ src/tests/apiDataToListState.test.js
@@ -9,19 +9,23 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import DonationList from '../components/DonationList';
 
-describe('DonationList API call on mount', async () => {
-  const donationListWrapper = await shallow(<DonationList />);
-  await donationListWrapper.update();
+describe('DonationList API call on mount', () => {
 
-  it('makes a call to the API on component mount and sets the state', () => {
+  it('makes a call to the API on component mount and sets the state', async () => {
+    const donationListWrapper = await shallow(<DonationList />);
+    await donationListWrapper.instance().componentWillMount();
     expect(donationListWrapper.state('donations').length).toEqual(1);
   });
 
-  it('formats the donation results in the state correctly', () => {
-    expect(donationListWrapper.state('donations')).toHaveProperty(amount, '10.00');
-    expect(donationListWrapper.state('donations')).toHaveProperty(donationDate, 'Thursday, 15/11/2018, 19:06');
-    expect(donationListWrapper.state('donations')).toHaveProperty(donorDisplayName, 'John Smith');
-    expect(donationListWrapper.state('donations')).toHaveProperty(imageUrl, 'https://www.justgiving.com/content/images/graphics/icons/avatars/facebook-avatar.gif');
-    expect(donationListWrapper.state('donations')).toHaveProperty(message, 'Lorem Ipsum');
+  it('formats the donation results in the state correctly', async () => {
+    const donationListWrapper = await shallow(<DonationList />);
+    await donationListWrapper.instance().componentWillMount();
+
+    const mockDonationValue = donationListWrapper.state('donations')[0];
+    expect(mockDonationValue).toHaveProperty('amount', '£10.00');
+    expect(mockDonationValue).toHaveProperty('donationDate', 'Monday, 01/10/2018, 01:00');
+    expect(mockDonationValue).toHaveProperty('donorDisplayName', 'John Smith');
+    expect(mockDonationValue).toHaveProperty('imageUrl', 'https://www.justgiving.com/content/images/graphics/icons/avatars/facebook-avatar.gif');
+    expect(mockDonationValue).toHaveProperty('message', 'Lorem Ipsum');
   });
 });
