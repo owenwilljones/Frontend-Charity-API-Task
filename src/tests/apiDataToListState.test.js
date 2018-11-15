533d1ae33c555ba4015a475f03845f803a41f837
Owen Jones <o-w-jones@outlook.com>
2018-11-15 21:08:06 +0000

    Update api test

--- src/tests/apiDataToListState.test.js
+++ src/tests/apiDataToListState.test.js
@@ -10,19 +10,18 @@ import {shallow} from 'enzyme';
 import DonationList from '../components/DonationList';
 
 describe('DonationList API call on mount', async () => {
-    const donationListWrapper = await shallow(<DonationList />);
-    await donationListWrapper.update();
+  const donationListWrapper = await shallow(<DonationList />);
+  await donationListWrapper.update();
 
-    it('makes a call to the API on component mount and sets the state', () => {
-        expect(donationListWrapper.state('donations').length).toEqual(1);
-    });
+  it('makes a call to the API on component mount and sets the state', () => {
+    expect(donationListWrapper.state('donations').length).toEqual(1);
+  });
 
-    it('formats the donation results in the state correctly', () => {
-        expect(donationListWrapper.state('donations')).toHaveProperty(amount, '10.00');
-        expect(donationListWrapper.state('donations')).toHaveProperty(currencyCode, 'GBP');
-        expect(donationListWrapper.state('donations')).toHaveProperty(donationDate, 'Thu Nov 15 2018 19:06:05 GMT');
-        expect(donationListWrapper.state('donations')).toHaveProperty(donorDisplayName, 'John Smith');
-        expect(donationListWrapper.state('donations')).toHaveProperty(imgUrl, 'https://www.justgiving.com/content/images/graphics/icons/avatars/facebook-avatar.gif');
-        expect(donationListWrapper.state('donations')).toHaveProperty(messageUrl, 'Lorem Ipsum');
-    });
+  it('formats the donation results in the state correctly', () => {
+    expect(donationListWrapper.state('donations')).toHaveProperty(amount, '10.00');
+    expect(donationListWrapper.state('donations')).toHaveProperty(donationDate, 'Thursday, 15/11/2018, 19:06:05 GMT');
+    expect(donationListWrapper.state('donations')).toHaveProperty(donorDisplayName, 'John Smith');
+    expect(donationListWrapper.state('donations')).toHaveProperty(imgUrl, 'https://www.justgiving.com/content/images/graphics/icons/avatars/facebook-avatar.gif');
+    expect(donationListWrapper.state('donations')).toHaveProperty(messageUrl, 'Lorem Ipsum');
+  });
 });
